cf89bdb7136788700d0b0f34ee6a378a8fe85ea3
Paulo Miranda <paulorobertom@gmail.com>
2022-10-23 20:54:48 -0300

    Add a React TS template

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+
+        <div id="d1" draggable="true">Drag me</div>
+        <div id="d2" draggable="true">Or me!</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
